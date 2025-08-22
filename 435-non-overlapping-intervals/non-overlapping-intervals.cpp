ac08587f37382cd9fb7d0e24c872dbc03f7740bb
Anam <87643183+AnamMalikk@users.noreply.github.com>
2025-08-22 22:00:41 +0530

    Time: 63 ms (29.12%) | Memory: 93.9 MB (87.96%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -3,28 +3,6 @@ public:
 // TC=O(N log N)
 // SC=O(1)
 
-// RULE: always choose the interval that ends first.
-// Reason:
-/* 
-We want to remove the minimum number of intervals so that the remaining intervals are non-overlapping.
-
-Equivalently, we can think:
-
-Maximize the number of non-overlapping intervals.
-
-The minimum number to remove = total intervals - maximum number of non-overlapping intervals.
-
-
-// So basically we need to maximize the no. of non overlapping intervals. That's why greedy works.
-// If we sort by start time, we might pick a long interval early, which blocks shorter intervals later.
-
-
-[1, 10], [2, 3], [3, 4]
-
-If you pick [1,10] first, you can't take any others because they will overlap with [1,10]
-If you pick [2,3] first (ends earlier), you can also take [3,4] because they don't overlap.
-
-*/
 
     static bool cmp(vector<int>&a,vector<int>&b)
     {
